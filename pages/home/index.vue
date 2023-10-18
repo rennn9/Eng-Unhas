083bc10328a0b45f0fdd17077e8c2515fdf57a24
Rendi Wijaya <wijayarendi9@gmail.com>
2023-10-19 07:50:58 +0800

    logo and navbar

---
+++ pages/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  
+    <header class="p-4  bg-[#063d63]">
+      <div class="container ">
+        <img src="@/assets/img/Logo_FTUH.png" alt="FTUH Logo" class="w-auto h-16 mx-[200PX]" />
+      </div>     
+    </header>
+
+    <nav class="bg-[#063d63]">
+      <ul class="flex mx-[215px] text-[11px] text-white">
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">PROFILE</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">AKADEMIK</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">SUMBER DAYA</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">K3</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">KEMAHASISWAAN</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">RIEST DAN INOVASI</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">KEMITRAAN</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">GPM-PR</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">COT</a></li>
+        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">DHARMA WANITA</a></li>
+      </ul>
+    </nav>
+
+  </template>
+
+
+
---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./layouts/**/*.vue",
+    "./pages/**/*.vue",
+    "./plugins/**/*.{js,ts}",
+    "./app.vue",
+  ],
+  theme: {
+
+  },
+  plugins: [],
+}
