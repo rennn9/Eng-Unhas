15fee2befa7b4c96f326e0ee80015304dfb245ff
Rendi Wijaya <wijayarendi9@gmail.com>
2023-10-19 10:02:19 +0800

    change img name

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,27 +1,33 @@
 <template>
   
-    <header class="p-4  bg-[#063d63]">
-      <div class="container ">
-        <img src="@/assets/img/Logo_FTUH.png" alt="FTUH Logo" class="w-auto h-16 mx-[200PX]" />
-      </div>     
+    <header class="  bg-[#063d63]">
+      <div class="container py-[20px]">
+        <img src="@/assets/img/Logo_FTUH.png" alt="FTUH Logo" class="w-auto h-16 mx-[200PX] " />
+      </div>
+      
+      <ul class="flex mx-[200px] text-[11px] text-white">
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">PROFILE</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">AKADEMIK</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">SUMBER DAYA</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">K3</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">KEMAHASISWAAN</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">RIEST DAN INOVASI</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">KEMITRAAN</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">GPM-PR</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">COT</a></li>
+        <li class="container w-auto px-[10px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">DHARMA WANITA</a></li>
+      </ul>
     </header>
 
-    <nav class="bg-[#063d63]">
-      <ul class="flex mx-[215px] text-[11px] text-white">
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">PROFILE</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">AKADEMIK</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">SUMBER DAYA</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">K3</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">KEMAHASISWAAN</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">RIEST DAN INOVASI</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">KEMITRAAN</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">GPM-PR</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">COT</a></li>
-        <li class="container w-auto px-[11px] py-[20px] hover:bg-[#EB2629] box-content"><a href="#">DHARMA WANITA</a></li>
-      </ul>
-    </nav>
 
+
+
+    
+    <img src="@/assets/img/ftuh.jpg" alt="Deskripsi gambar" class="w-full bg-center bg-cover aspect-ratio-16/9">
+    <!-- <div class="w-full aspect-ratio-16/9 h-[200px] bg-center" style="background-image: url('@/assets/img/carousel-1.jpeg'); background-size: cover;"></div> -->
+    <!-- <div class="relative w-full h-0 aspect-w-16 aspect-h-9">
+      <img src="@/assets/img/ftuh.jpg" alt="Deskripsi gambar" class="absolute inset-0 object-cover w-full h-full" />
+    </div> -->
+
+    
   </template>
-
-
-
