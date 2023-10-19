ce30c8d5bc8f8eb708d364aa78f80467028010ee
Rendi Wijaya <wijayarendi9@gmail.com>
2023-10-19 10:59:14 +0800

    add header image

--- pages/home/index.vue
+++ pages/home/index.vue
@@ -1,5 +1,4 @@
 <template>
-  
     <header class="  bg-[#063d63]">
       <div class="container py-[20px]">
         <img src="@/assets/img/Logo_FTUH.png" alt="FTUH Logo" class="w-auto h-16 mx-[200PX] " />
@@ -19,15 +18,23 @@
       </ul>
     </header>
 
+    <div class="aspect-ratio-box">
+      <img src="@/assets/img/ftuh.jpg" alt="Gambar" class="object-cover w-full h-full">
+    </div>
+</template>
 
+  <style>
+    .aspect-ratio-box {
+      position: relative;
+      width: 100%;
+      padding-bottom: 56.25%;
+    }
 
-
-    
-    <img src="@/assets/img/ftuh.jpg" alt="Deskripsi gambar" class="w-full bg-center bg-cover aspect-ratio-16/9">
-    <!-- <div class="w-full aspect-ratio-16/9 h-[200px] bg-center" style="background-image: url('@/assets/img/carousel-1.jpeg'); background-size: cover;"></div> -->
-    <!-- <div class="relative w-full h-0 aspect-w-16 aspect-h-9">
-      <img src="@/assets/img/ftuh.jpg" alt="Deskripsi gambar" class="absolute inset-0 object-cover w-full h-full" />
-    </div> -->
-
-    
-  </template>
+    .aspect-ratio-box img {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 77%;
+    }
+  </style>
