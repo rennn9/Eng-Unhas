8f293d4efb82fa4866e7074751e7fa5e3c91c120
Rendi Wijaya <wijayarendi9@gmail.com>
2023-12-12 12:09:47 +0800

    fix mobile responsive

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <script></script>
 
 <template>
-  <Header class="flex mb-0 md:mb-[12vw]"></Header>
+  <Header class="flex mb-0 md:mb-[120px]"></Header>
   <div id="informasi-dan-berita" class="flex flex-col md:flex-row lg:mx-[273PX] md:mx-[100px] mx-[25px] lg:my-[87px] md:my-[57px] sm:my-[27px]">
     <Berita class="mb-10 md:mr-4 md:mb-0"></Berita>
     <Informasi class="mb-10 md:mb-0"></Informasi>
--- components/Footer.vue
+++ components/Footer.vue
@@ -7,39 +7,39 @@ export default {
 <template>
   <footer class="">
     <div class="bg-[#063d63]">
-      <div class="mx-[200px] py-[50px] flex flex-col">
+      <div class="mx-[20px] md:mx-[200px] py-[50px] flex flex-col">
         <div class="pb-[30px]">
           <div class="gcse-search"></div>
         </div>
-        <div class="flex justify-around">
-          <div class="flex flex-col gap-[20px]">
+        <div class="flex flex-col md:flex-row md:justify-around">
+          <div class="flex flex-col mb-[20px] gap-[20px]">
             <div class="flex flex-col">
-              <span class="inline-block uppercase text-white">Alamat</span>
+              <span class="inline-block uppercase text-white font-bold">Alamat</span>
               <span class="inline-block text-white">
                 Jalan Poros Malino Km. 6, Bontomarannu. Gowa, Sulawesi Selatan.
                 92171</span
               >
             </div>
             <div class="flex flex-col">
-              <span class="inline-block uppercase text-white"
+              <span class="inline-block uppercase text-white font-bold"
                 >Kontak & Email</span
               >
               <span class="inline-block text-white">+62 852-9999-9958</span>
               <span class="inline-block text-white"> teknik@unhas.ac.id</span>
             </div>
           </div>
-          <div class="flex flex-col gap-[20px]">
-            <span class="inline-block uppercase text-white"
+          <div class="flex flex-col mb-[20px] md:gap-[20px]">
+            <span class="inline-block uppercase text-white font-bold"
               >Statistik Website</span
             >
             <img
               src="@/assets/img/flag_counter.png"
               alt="visitors by country"
-              class="inline-block"
+              class="inline-block w-[125px]"
             />
           </div>
-          <div class="flex flex-col gap-[20px]">
-            <span class="inline-block uppercase text-white">Didukung oleh</span>
+          <div class="flex flex-col md:gap-[20px]">
+            <span class="inline-block uppercase text-white font-bold">Didukung oleh</span>
             <div class="flex gap-[20px]">
               <img
                 src="@/assets/img/upana.png"
